d831d6bdfb76cbd3ee6545eaa3d2b6c9a978345c
Edivan Carvalho <edivancarvalho2008@hotmail.com>
2020-07-10 01:20:38 -0300

    Update Comandos.sql

--- Comandos.sql
+++ Comandos.sql
@@ -3,7 +3,7 @@
 create database dbinfox;
 -- a linha abaixo escolhe o banco de dados;
 use dbinfox;
--- o bloco de instruuções abaixo cria uma tabela;
+-- o bloco de instruções abaixo cria uma tabela;
 create table tbusuarios(
 iduser int primary key,
 usuario varchar(50) not null,
@@ -32,3 +32,50 @@ update tbusuarios set fone='1122-3344' where iduser=3;
 -- a linha abaixo apaga um registro da tabela. (CRUD)
 -- delete --> delete
 delete from tbusuarios where iduser=3;
+
+
+select * from tbusuarios;
+
+create table tbclientes(
+idcli int primary key auto_increment,
+nomecli varchar(50) not null,
+endcli varchar(100),
+foneli varchar(50) not null,
+emailcli varchar(50)
+);
+
+describe tbclientes;
+
+insert into tbclientes(nomecli,endcli,fonecli,emailcli)
+values('Edivan carvallho','rua santa','4444-4444','ed@local.com');
+
+select * from tbclientes;
+
+use dbinfox;
+
+create table tbos(
+os int primary key auto_increment,
+data_os timestamp default current_timestamp,
+equipamento varchar(150) not null,
+defeito varchar(150) not null,
+servico varchar(150),
+tecnico varchar(30),
+valor decimal(10,2),
+idcli int not null,
+foreign key(idcli) references tbclientes(idcli)
+);
+ 
+describe tbos;
+-- faz a união de duas tabelas
+insert into tbos(equipamento,defeito,servico,tecnico,valor,idcli)
+values ('Notebook','não liga','troca da fonte','Pedro','90.00',1);
+
+select * from tbos;
+
+-- o código abaixo traz informações de duas tabelas;
+select
+O.os,equipamento,defeito,servico,valor,
+C.nomecli,fonecli
+from tbos as O
+inner join tbclientes as C
+on (O.idcli = C.idcli);
